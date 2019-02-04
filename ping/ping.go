b89cca81561f256e0c2fc08547795b8198e5f495
arifsetiawan <n.arif.setiawan@gmail.com>
2019-02-04 09:57:18 +0700

    ping return body

--- ping/ping.go
+++ ping/ping.go
@@ -1,15 +1,22 @@
 package ping
 
 import (
+	"io/ioutil"
 	"net/http"
 	"time"
 )
 
 // CheckURL is
-func CheckURL(url string, timeout time.Duration) (string, error) {
+func CheckURL(url string, timeout time.Duration) (string, int, error) {
 	client := http.Client{
 		Timeout: timeout,
 	}
-	_, err := client.Get(url)
-	return "", err
+	resp, err := client.Get(url)
+	if err != nil {
+		return "", http.StatusInternalServerError, err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	return string(bodyBytes), resp.StatusCode, nil
 }
